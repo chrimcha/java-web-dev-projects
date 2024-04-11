784c064ad60c1bb2960c315babc3e30a6b226e0b
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-04-11 00:27:55 -0500

    ch 4 studio prework set up with last weeks planning

--- classes-part-2/studio/src/main/java/org/launchcode/Menu.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Menu.java
@@ -2,16 +2,26 @@ package org.launchcode;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 
 public class Menu {
-    private Date lastUpdated;
+    private Date lastUpdated = new Date();
     private ArrayList<MenuItem> items;
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
-        this.lastUpdated = d;
-        this.items = i;
+    //contractor
+    public Menu(Date aDate, ArrayList<MenuItem> aItem) {
+        this.lastUpdated = aDate;
+        this.items = aItem;
     }
 
+    public Menu(ArrayList<MenuItem> aItem) {
+        this.items = aItem;
+    }
+
+    //methods
+
+
+    //getters and setters
     public void setLastUpdated(Date lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
@@ -26,6 +36,26 @@ public class Menu {
 
     public ArrayList<MenuItem> getItems() {
         return items;
+    }
+
+    //overrides
+    @Override
+    public String toString() {
+        return "Menu was Last Updated on " + lastUpdated +
+                "\nWith these items: " + items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return Objects.equals(items, menu.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(items);
     }
 }
 
--- classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
@@ -1,32 +1,86 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class MenuItem {
+    private String name;
     private double price;
     private String description;
     private String category;
-    private boolean isNew;
+    private boolean isNew = true;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
-        this.price = p;
-        this.description = d;
-        this.category = c;
-        this.isNew = iN;
+    //constructor
+
+    //use if item is new
+    public MenuItem(String aName, double aPrice, String aDescription, String aCategory) {
+        this.name = aName;
+        this.price = aPrice;
+        this.description = aDescription;
+        this.category = aCategory;
     }
 
+    //use if item is not new
+    public MenuItem(String aName, double aPrice, String aDescription, String aCategory, boolean aIsNew) {
+        this.name = aName;
+        this.price = aPrice;
+        this.description = aDescription;
+        this.category = aCategory;
+        this.isNew = aIsNew;
+    }
+
+    //methods
+
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
     public void setPrice(double price) {
         this.price = price;
     }
-
+    public String getDescription() {
+        return description;
+    }
     public void setDescription(String description) {
         this.description = description;
     }
-
-    public void setCategory(String category) {
-        this.category = category;
+    public String getCategory() {
+        return category;
     }
-
+    public boolean isNew() {
+        return isNew;
+    }
     public void setNew(boolean aNew) {
         isNew = aNew;
+    }
+
+    //overrides
+    @Override
+    public String toString() {
+        return "\n##########################################" +
+                "\nMenuItem: " + name +
+                "\nPrice: " + price +
+                "\nDescription: " + description +
+                "\nCategory: " + category +
+                "\nIs It New?: " + isNew
+                + "\n##########################################";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return Double.compare(price, menuItem.price) == 0 && Objects.equals(description, menuItem.description) && Objects.equals(category, menuItem.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price, description, category);
     }
 }
 
